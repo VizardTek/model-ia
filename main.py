7521c4ee82fb6589eadced14d80ab5b1ce7d0fd4
Noubouille <secondesu@gmail.com>
2024-04-19 11:11:02 +0200

    push ai model

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import cv2
 from ultralytics import YOLO
 import math
+import requests
 
 # Load your trained model
-model = YOLO('best.pt')
+model = YOLO('best2.pt')
 
 # Open a video file
-video_path = 'fire.mp4'
-cap = cv2.VideoCapture(video_path)
+  
+cap = cv2.VideoCapture(0)  # Read the video file
+# video_path = 'fire.mp4'
+# cap = cv2.VideoCapture(video_path)
 
 frame_rate = cap.get(cv2.CAP_PROP_FPS)
-counter = 0
+# counter = 0``
+frame_skip = 30  # Number of frames to skip
+frame_count = 30  # Frame counter
 
 classNames = ['fire', 'other', 'smoke']
 
@@ -41,12 +46,13 @@ else:
                 cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
                 # confidence
-                confidence = math.ceil((box.conf[0] * 100)) / 100
-                print("Confidence --->", confidence)
+                confidence = math.ceil((box.conf[0] * 100))
+                # print("Confidence --->", confidence)
 
                 # class name
                 cls = int(box.cls[0])
-                print("Class name -->", classNames[cls])
+
+                # print("Class name -->", classNames[cls])
 
                     # object details
                 org = [x1, y1]
@@ -55,7 +61,34 @@ else:
                 color = (255, 0, 0)
                 thickness = 2
 
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+                frame_count += 1
+                set_fire = False
+                if classNames[cls] != 'other':
+                    if classNames[cls] == 'fire':
+                        set_fire = True
+
+                        if set_fire:
+                            print("je passe tout les 30")
+                            cv2.putText(img, classNames[cls] + " " + str(confidence), org, font, fontScale, color, thickness)
+                            print("feu detecté")
+
+                            headers = {
+                                'Content-Type': 'application/json',
+                                "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjVmYWUzMGY0MDJmYjBiMGVkYTEyNWY1IiwiX2lkIjoiNjVmYWUzMGQ0MDJmYjBiMGVkYTEyNWVmIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzEwOTQxMTI5LCJleHAiOjE3MTExMTM5Mjl9.vWV2PXix2t3MK5nf-RO7rHqbzyzI4fTI7szKJ3V99MY'
+                            }
+
+                            data = {
+                                'id': frame_count, 
+                                "alertData": { "title": "ALERTE FEU", "body": f"ALERTE FEU DETECTE AVEC UNE CONFIANCE DE {confidence}%"}
+                            }
+
+
+                            print(requests.post('http://192.168.137.1:4000/api/alerting/alert', json=data, headers=headers).json())
+                              
+                    pass
+                    
+
+                # cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
 
             out.write(img)
             if cv2.waitKey(1) == ord('q'):
