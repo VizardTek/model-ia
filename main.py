676689a52e51c9f04d7603c5f14888ba635d2cd1
Noubouille <secondesu@gmail.com>
2024-05-31 15:59:36 +0200

    edit detection script with new model and request to backend when fire detected

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ import math
 import requests
 
 # Load your trained model
-model = YOLO('best2.pt')
+model = YOLO('best-fire-smoke-only.pt')
 
 # Open a video file
   
 cap = cv2.VideoCapture(0)  # Read the video file
-# video_path = 'fire.mp4'
-# cap = cv2.VideoCapture(video_path)
 
 frame_rate = cap.get(cv2.CAP_PROP_FPS)
 # counter = 0``
 frame_skip = 30  # Number of frames to skip
 frame_count = 30  # Frame counter
-
-classNames = ['fire', 'other', 'smoke']
+count = 0
+countNoFire = 0
+classNames = ['Fire', 'clear', 'fire', 'nf', 'smoke']
 
 if not cap.isOpened():
     print("Error opening video file")
 else:
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
-    out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+    out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
 
     while True:
         success, img = cap.read()
@@ -32,7 +31,7 @@ else:
             break  # If no frame is read, end of video is reached
 
         results = model(img, stream=True)
-
+        
          # coordinates
         for r in results:
             boxes = r.boxes
@@ -47,13 +46,9 @@ else:
 
                 # confidence
                 confidence = math.ceil((box.conf[0] * 100))
-                # print("Confidence --->", confidence)
 
                 # class name
                 cls = int(box.cls[0])
-
-                # print("Class name -->", classNames[cls])
-
                     # object details
                 org = [x1, y1]
                 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -63,33 +58,34 @@ else:
 
                 frame_count += 1
                 set_fire = False
-                if classNames[cls] != 'other':
-                    if classNames[cls] == 'fire':
-                        set_fire = True
+                cv2.putText(img, classNames[cls] + " " + str(confidence), org, font, fontScale, color, thickness)
 
-                        if set_fire:
-                            print("je passe tout les 30")
-                            cv2.putText(img, classNames[cls] + " " + str(confidence), org, font, fontScale, color, thickness)
+                if classNames[cls] == 'fire':
+                    set_fire = True
+
+                    if set_fire:
+                        print('COUNTTTTTTTT ',count)
+                        cv2.putText(img, classNames[cls] + " " + str(confidence), org, font, fontScale, color, thickness)
+                        if confidence > 70 :
+                            count = count + 1
                             print("feu detecté")
+                            if count == 10:
+                                print('SEND ALERTTTT')
+                                headers = {
+                                    'Content-Type': 'application/json',
+                                    "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjY1OTkxYWU3ODI3ZTI0NGNjNjgwMzVlIiwiX2lkIjoiNjY1OTgzMGU3ODI3ZTI0NGNjNjdmZWVlIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzE3MTQ2MDMwLCJleHAiOjE3MTczMTg4MzB9.E4G2NE624sS-JiGo788RoBTBbdGgtXIcPUZ-CIe1Yr8'
+                                }
 
-                            headers = {
-                                'Content-Type': 'application/json',
-                                "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjVmYWUzMGY0MDJmYjBiMGVkYTEyNWY1IiwiX2lkIjoiNjVmYWUzMGQ0MDJmYjBiMGVkYTEyNWVmIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzEwOTQxMTI5LCJleHAiOjE3MTExMTM5Mjl9.vWV2PXix2t3MK5nf-RO7rHqbzyzI4fTI7szKJ3V99MY'
-                            }
+                                data = {
+                                    'id': frame_count, 
+                                    "alertData": { "title": "ALERTE FEU", "body": f"ALERTE FEU DETECTE AVEC UNE CONFIANCE DE {confidence}%"}
+                                }
 
-                            data = {
-                                'id': frame_count, 
-                                "alertData": { "title": "ALERTE FEU", "body": f"ALERTE FEU DETECTE AVEC UNE CONFIANCE DE {confidence}%"}
-                            }
+                                print(requests.post('http://192.168.137.1:4000/api/alerting/alert', json=data, headers=headers).json())
 
-
-                            print(requests.post('http://192.168.137.1:4000/api/alerting/alert', json=data, headers=headers).json())
-                              
-                    pass
-                    
-
-                # cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-
+                            elif count > 10:
+                                count = 0
+                pass
             out.write(img)
             if cv2.waitKey(1) == ord('q'):
                 break
